af696e65de06f05fcfa6ddd40a617592130b3141
Eustaquio <cyrilleustaquio111222@gmail.com>
2024-10-21 15:11:23 +0800

    automatically select device

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -2,7 +2,10 @@ import torch
 from utils.face_recognition import get_model
 
 def load_face_recognition_model(model_name="edgeface_s_gamma_05", model_path=".models/edgeface_s_gamma_05.pt"):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     model = get_model(model_name)
-    model.load_state_dict(torch.load(model_path, map_location='cpu'))
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.to(device)
     model.eval()
-    return model
+    return model, device 
