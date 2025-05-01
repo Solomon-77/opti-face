27b1e3aef4c28d0fe2ae52f7f62fd29bfb0fb60f
Eustaquio <cyrilleustaquio111222@gmail.com>
2025-05-01 14:17:18 +0800

    update scrfd

--- src/backend/utils/scrfd.py
+++ src/backend/utils/scrfd.py
@@ -19,10 +19,10 @@ def distance2bbox(points, distance, max_shape=None):
     x2 = points[:, 0] + distance[:, 2]
     y2 = points[:, 1] + distance[:, 3]
     if max_shape is not None:
-        x1 = x1.clamp(min=0, max=max_shape[1])
-        y1 = y1.clamp(min=0, max=max_shape[0])
-        x2 = x2.clamp(min=0, max=max_shape[1])
-        y2 = y2.clamp(min=0, max=max_shape[0])
+        x1 = np.clip(x1, 0, max_shape[1])
+        y1 = np.clip(y1, 0, max_shape[0])
+        x2 = np.clip(x2, 0, max_shape[1])
+        y2 = np.clip(y2, 0, max_shape[0])
     return np.stack([x1, y1, x2, y2], axis=-1)
 
 def distance2kps(points, distance, max_shape=None):
@@ -42,8 +42,8 @@ def distance2kps(points, distance, max_shape=None):
         px = points[:, i % 2] + distance[:, i]
         py = points[:, i % 2 + 1] + distance[:, i + 1]
         if max_shape is not None:
-            px = px.clamp(min=0, max=max_shape[1])
-            py = py.clamp(min=0, max=max_shape[0])
+            px = np.clip(px, 0, max_shape[1])
+            py = np.clip(py, 0, max_shape[0])
         preds.append(px)
         preds.append(py)
     return np.stack(preds, axis=-1)
